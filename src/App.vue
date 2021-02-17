f7d2459d83a06f7e0fc9fd06e6414663ff7a4f8d
mFuscolin <mfuscolin@gmail.com>
2021-02-16 21:44:22 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1 class="text-center">Conversor de Moedas</h1>
+    <div class="linha"> 
+    <Conversor moedaA="USD" moedaB="BRL"/>
+     <Conversor moedaA="BRL" moedaB="USD"/>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+import Conversor from "./components/Conversor.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: auto;
+  max-width: 900px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.linha{
+  margin: top;
+  display: flex;
+  justify-content: space-around;
+}
+.text-center{
+  text-align: center;
+}
+</style>
